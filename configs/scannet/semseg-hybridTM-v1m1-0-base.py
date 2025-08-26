1b8a01a49ffee33b763fe8d8ca3f94ef762596e7
cugitee <2289068115@qq.com>
2025-08-26 17:39:43 +0800

    11

--- configs/scannet/semseg-hybridTM-v1m1-0-base.py
+++ configs/scannet/semseg-hybridTM-v1m1-0-base.py
@@ -1,7 +1,7 @@
 _base_ = ["../_base_/default_runtime.py"]
 
 # misc custom setting
-batch_size = 2  # bs: total bs in all gpus
+batch_size = 3  # bs: total bs in all gpus
 num_worker = 0
 mix_prob = 0.8
 empty_cache = False
