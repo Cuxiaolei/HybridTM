7953fd05b85c3eb657eb2610d043f86ed1ea0e44
cugitee <2289068115@qq.com>
2025-08-26 12:54:13 +0800

    11

--- configs/scannet/semseg-pt-v3m1-0-base.py
+++ configs/scannet/semseg-pt-v3m1-0-base.py
@@ -102,7 +102,7 @@ data = dict(
                 mode="train",
                 return_grid_coord=True,
             ),
-            dict(type="SphereCrop", point_max=102400, mode="random"),
+            dict(type="SphereCrop", point_max=10000, mode="random"),
             dict(type="CenterShift", apply_z=False),
             dict(type="NormalizeColor"),
             # dict(type="ShufflePoint"),
